526bf3ac1550f2246862ac3c4055adefe8425fc1
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-08-15 21:08:47 +0600

    change some classes

--- src/pages/BestDeal/BestDeal.jsx
+++ src/pages/BestDeal/BestDeal.jsx
@@ -1,12 +1,9 @@
-import React, { useRef, useState } from 'react';
-// Import Swiper React components
+/
 import { Swiper, SwiperSlide } from 'swiper/react';
 
-// Import Swiper styles
+
 import 'swiper/css';
 import 'swiper/css/pagination';
-
-import './styles.css';
 
 // import required modules
 import { Pagination } from 'swiper/modules';
