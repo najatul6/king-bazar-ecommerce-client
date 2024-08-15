a9e5fe3e9517239c1280a292970075846a191e17
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-08-15 21:22:34 +0600

    add heading in faq section

--- src/common/Faq/Faq.jsx
+++ src/common/Faq/Faq.jsx
@@ -1,12 +1,20 @@
-
-
 export const Faq = () => {
   return (
-    <div tabIndex={0} className="collapse collapse-plus border-base-300 bg-base-200 border">
-    <div className="collapse-title text-xl font-medium">Focus me to see content</div>
-    <div className="collapse-content">
-      <p>tabindex={0} attribute is necessary to make the div focusable</p>
+    <div>
+      <h1 className="text-3xl font-medium text-center py-10">FAQ</h1>
+      
+
+      <div
+        tabIndex={0}
+        className="collapse collapse-plus border-base-300 bg-base-200 border"
+      >
+        <div className="collapse-title text-xl font-medium">
+          Focus me to see content
+        </div>
+        <div className="collapse-content">
+          <p>tabindex={0} attribute is necessary to make the div focusable</p>
+        </div>
+      </div>
     </div>
-  </div>
-  )
-}
+  );
+};
