48a2f9c214ab93d8a4d33cdfb786476a2459ea59
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-08-24 22:31:55 +0600

    items map set up completely

--- src/common/BestDealCard/BestDealCard.jsx
+++ src/common/BestDealCard/BestDealCard.jsx
@@ -1,35 +1,10 @@
-import { Swiper, SwiperSlide } from 'swiper/react';
 
-
-import 'swiper/css';
-import 'swiper/css/pagination';
-
-// import required modules
-import { Pagination } from 'swiper/modules';
 
 
 const BestDealCard = () => {
   return (
     <div>
-         <Swiper
-        slidesPerView={3}
-        spaceBetween={30}
-        pagination={{
-          clickable: true,
-        }}
-        modules={[Pagination]}
-        className="mySwiper"
-      >
-        <SwiperSlide>Slide 1</SwiperSlide>
-        <SwiperSlide>Slide 2</SwiperSlide>
-        <SwiperSlide>Slide 3</SwiperSlide>
-        <SwiperSlide>Slide 4</SwiperSlide>
-        <SwiperSlide>Slide 5</SwiperSlide>
-        <SwiperSlide>Slide 6</SwiperSlide>
-        <SwiperSlide>Slide 7</SwiperSlide>
-        <SwiperSlide>Slide 8</SwiperSlide>
-        <SwiperSlide>Slide 9</SwiperSlide>
-      </Swiper>
+        
     </div>
   )
 }
--- src/pages/BestDeal/BestDeal.jsx
+++ src/pages/BestDeal/BestDeal.jsx
@@ -28,6 +28,11 @@ const BestDeal = () => {
         modules={[Pagination]}
         className="mySwiper"
       >
+        {bestProducts?.map(item=>{
+          return(
+            <SwiperSlide key={item?._id}>Slide 1</SwiperSlide>
+          )
+        })}
         <SwiperSlide>Slide 1</SwiperSlide>
         <SwiperSlide>Slide 2</SwiperSlide>
         <SwiperSlide>Slide 3</SwiperSlide>
