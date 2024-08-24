bae5ed38575333d4cad909af1200e26d36d913c8
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-08-24 22:40:48 +0600

    set image for best deals

--- src/pages/BestDeal/BestDealCard/BestDealCard.jsx
+++ src/pages/BestDeal/BestDealCard/BestDealCard.jsx
@@ -3,7 +3,9 @@ const BestDealCard = ({item}) => {
     const {brand,product_name,category,discount_price,price,features,rating,stock_availability,image_url}=item;
     return (
         <div>
-            
+            <div>
+                <img src={image_url} alt={product_name} className="w-full" />
+            </div>
         </div>
     );
 };
